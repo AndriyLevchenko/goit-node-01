aad9cb593523e476f2b42098c1e395f12fab0d01
AndriyLevchenko <98983557+AndriyLevchenko@users.noreply.github.com>
2023-01-02 18:27:52 +0200

    2

--- contacts.js
+++ contacts.js
@@ -1,24 +1,71 @@
+const nanoid = require('nanoid');
+
 const fs = require('fs').promises;
 const path = require('path');
 
 const contactsPath = path.resolve('./db/contacts.json');
 
-fs.readFile(filename, [options])
-
-fs.writeFile(filename, data, [options])
-
-function listContacts() {
-
+async function listContacts() {
+    try {
+        const data = await fs.readFile(contactsPath, "utf-8");
+        const parsedData = await JSON.parse(data);
+        console.log(parsedData);
+    } catch (e) {
+        console.error(e);
+    }
 }
   
-function getContactById(contactId) {
-    // ...твій код
+async function getContactById(contactId) {
+    try {
+        const data = await fs.readFile(contactsPath, "utf-8");
+        const parsedData = await JSON.parse(data);
+        const result = await parsedData.find(
+          (contact) => Number(contact.id) === contactId
+        );
+        console.log(result);
+    } catch (e) {
+        console.error(e);
+    }
 }
   
-function removeContact(contactId) {
-    // ...твій код
+async function removeContact(contactId) {
+    try {
+        const data = await fs.readFile(contactsPath, "utf-8");
+        const parsedData = await JSON.parse(data);
+        const delContact = await parsedData.find(
+          (contact) => Number(contact.id) === contactId
+        );
+        if (delContact) {
+          const index = parsedData.indexOf(delContact);
+          parsedData.splice(index, 1);
+          await fs.writeFile(contactsPath, JSON.stringify(parsedData), "utf-8");
+        } else {
+          return console.log(`Contact with id=${contactId} not found!!!`);
+        }
+        console.log(`Contact with id=${contactId} is removed`);
+    } catch (e) {
+        console.error(e);
+    }
 }
   
-function addContact(name, email, phone) {
-    // ...твій код
+async function addContact(name, email, phone) {
+    try {
+        const data = await fs.readFile(contactsPath, "utf-8");
+        const parsedData = await JSON.parse(data);
+        const id = await nanoid();
+    
+        const newContact = { id, name, email, phone };
+        const updateContacts = [...parsedData, newContact];
+        await fs.writeFile(contactsPath, JSON.stringify(updateContacts), "utf-8");
+        await console.log(`Contact with id=${id} is added`);
+    } catch (e) {
+        console.error(e);
+    }
 }
+
+module.exports = {
+    listContacts,
+    getContactById,
+    removeContact,
+    addContact,
+  };
